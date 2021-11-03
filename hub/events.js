4bd147fde8e538ce0a8d19d5daafc05df8f96a6b
AntoineCharette <antoineocharette@gmail.com>
2021-11-02 23:33:30 -0400

    adds socket implementation to event driven programming

--- hub/events.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const Events = require('events');
-const deliveryEvents = new Events();
-
-module.exports = deliveryEvents;
--- hub/server.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const events = require('./events.js');
-const logEvent = require('./logEvent.js');
-
-events.on('pickup', logEvent('pickup'));
-events.on('in-transit', logEvent('in-transit'));
-events.on('delivered', logEvent('delivered'));
